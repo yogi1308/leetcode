3a105e0f89b24418fe96fd92553282e61555fb08
yogi1308 <shreetej.yogi@gmail.com>
2026-01-04 23:29:16 -0700

    Time: 27 ms (44.61%) | Memory: 17.8 MB (63.10%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.py
+++ 567-permutation-in-string/permutation-in-string.py
@@ -1,16 +1,35 @@
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
-        s1map = {}
-        for char in s1:
-            s1map[char] = s1map.get(char, 0) + 1
+        if len(s1) > len(s2): return False
+
+        s1Count, s2Count = {}, {}
+        for i in range(26):
+            s1Count[chr(97 + i)] = 0
+            s2Count[chr(97 + i)] = 0
+        for i in range(len(s1)):
+            s1Count[s1[i]] += 1
+            s2Count[s2[i]] += 1
         
+        matches = 0
+        for i in s1Count:
+            if s1Count[i] == s2Count[i]: matches += 1
+
         l = 0
         for r in range(len(s1), len(s2) + 1):
-            valid = True    
-            for char in s1map:
-                if s1map[char] != s2[l : r].count(char):
-                    valid = False
-            if valid:
-                return True
+            substr = s2[l : r]
+
+            if l != 0:
+                matched_earlier = s2Count[substr[-1]] == s1Count[substr[-1]]
+                s2Count[substr[-1]] = s2Count[substr[-1]] + 1
+                if s2Count[substr[-1]] == s1Count[substr[-1]]: matches += 1
+                elif matched_earlier: matches -= 1
+
+            if matches == 26: return True
+            
+            matched_earlier = s2Count[substr[0]] == s1Count[substr[0]]
+            s2Count[substr[0]] = s2Count[substr[0]] - 1
+            if s2Count[substr[0]] == s1Count[substr[0]]: matches += 1
+            elif matched_earlier: matches -= 1
+
             l += 1
         return False
