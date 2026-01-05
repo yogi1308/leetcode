3b45b3aec32f5dc092da582ff63a9f641b7c8982
yogi1308 <shreetej.yogi@gmail.com>
2026-01-04 21:58:07 -0700

    Time: 739 ms (16.87%) | Memory: 17.5 MB (86.40%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.py
+++ 567-permutation-in-string/permutation-in-string.py
@@ -2,17 +2,13 @@ class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
         s1map = {}
         for char in s1:
-            if char in s1map:
-                s1map[char] = s1map[char] + 1
-            else:
-                s1map[char] = 1
+            s1map[char] = s1map.get(char, 0) + 1
         
         l = 0
         for r in range(len(s1), len(s2) + 1):
-            substr = s2[l : r]
             valid = True    
             for char in s1map:
-                if s1map[char] != substr.count(char):
+                if s1map[char] != s2[l : r].count(char):
                     valid = False
             if valid:
                 return True
