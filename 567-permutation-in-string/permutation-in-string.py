44669cb072c3a0a89b2c6c80ab1f9ca930a16995
yogi1308 <shreetej.yogi@gmail.com>
2026-01-04 23:40:49 -0700

    Time: 23 ms (52.13%) | Memory: 17.7 MB (63.10%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.py
+++ 567-permutation-in-string/permutation-in-string.py
@@ -1,34 +1,33 @@
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
-        if len(s1) > len(s2):
-            return False
+        if len(s1) > len(s2): return False
 
-        s1Count, s2Count = [0] * 26, [0] * 26
-        for i in range(len(s1)):
-            s1Count[ord(s1[i]) - ord('a')] += 1
-            s2Count[ord(s2[i]) - ord('a')] += 1
-
-        matches = 0
+        s1Count, s2Count = {}, {}
         for i in range(26):
-            matches += (1 if s1Count[i] == s2Count[i] else 0)
+            s1Count[chr(97 + i)] = 0
+            s2Count[chr(97 + i)] = 0
+        for i in range(len(s1)):
+            s1Count[s1[i]] += 1
+            s2Count[s2[i]] += 1
+        
+        matches = 0
+        for i in s1Count:
+            if s1Count[i] == s2Count[i]: matches += 1
 
         l = 0
-        for r in range(len(s1), len(s2)):
-            if matches == 26:
-                return True
+        for r in range(len(s1), len(s2) + 1):
+            substr = s2[l : r]
 
-            index = ord(s2[r]) - ord('a')
-            s2Count[index] += 1
-            if s1Count[index] == s2Count[index]:
-                matches += 1
-            elif s1Count[index] + 1 == s2Count[index]:
-                matches -= 1
+            if l != 0:
+                s2Count[substr[-1]] = s2Count[substr[-1]] + 1
+                if s2Count[substr[-1]] == s1Count[substr[-1]]: matches += 1
+                elif s2Count[substr[-1]] - 1== s1Count[substr[-1]]: matches -= 1
 
-            index = ord(s2[l]) - ord('a')
-            s2Count[index] -= 1
-            if s1Count[index] == s2Count[index]:
-                matches += 1
-            elif s1Count[index] - 1 == s2Count[index]:
-                matches -= 1
+            if matches == 26: return True
+            
+            s2Count[substr[0]] = s2Count[substr[0]] - 1
+            if s2Count[substr[0]] == s1Count[substr[0]]: matches += 1
+            elif s2Count[substr[0]] + 1 == s1Count[substr[0]]: matches -= 1
+
             l += 1
-        return matches == 26
+        return False
