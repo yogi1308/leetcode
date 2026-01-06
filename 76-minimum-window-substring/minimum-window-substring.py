2debd97f0118d9cdf821bdaf32a70f1f465300c5
Shreetej Hadge <shreetejhadge1308@gmail.com>
2026-01-05 18:33:06 -0700

    docs(solution): Add commented-out previous solution attempts for reference

--- 76-minimum-window-substring/minimum-window-substring.py
+++ 76-minimum-window-substring/minimum-window-substring.py
@@ -33,3 +33,74 @@ class Solution:
                 l += 1
 
         return s[shortest_start : shortest_end]
+
+
+# original brute force Solution
+# class Solution:
+#     def minWindow(self, s: str, t: str) -> str:
+#         if s == t: return s
+#         shortest = len(s)
+#         shortestStr = ""
+#         tmap = {}
+#         for char in t:
+#             tmap[char] = tmap.get(char, 0) + 1
+
+#         for l in range(len(s) - len(t) + 1):
+#             for r in range(len(t) + l, len(s) + 1):
+#                 substr = s[l : r]
+#                 valid = True
+#                 print(substr)
+
+#                 for char in tmap.keys():
+#                     if tmap[char] > substr.count(char):
+#                         valid = False
+#                         break
+#                 if valid and len(substr) <= shortest:
+#                     shortest = len(substr)
+#                     shortestStr = substr
+#         return shortestStr
+
+
+sliding window Solution
+# class Solution:
+#     def minWindow(self, s: str, t: str) -> str:
+#         if s == t: return s
+#         shortest = len(s)
+#         shortestStr = ""
+#         tmap = {}
+#         smap = {}
+#         for i in range(26):
+#             tmap[chr(65 + i)] = 0
+#             smap[chr(65 + i)] = 0
+#             tmap[chr(97 + i)] = 0
+#             smap[chr(97 + i)] = 0
+
+#         for char in t:
+#             tmap[char] = tmap.get(char) + 1
+
+#         for l in range(len(s) - len(t) + 1):
+#             matches = 0
+#             smap = {key: 0 for key in smap}
+#             for char in s[l : len(t) + l]:
+#                 smap[char] = smap.get(char) + 1
+#             for char in tmap.keys():
+#                 if tmap[char] <= smap[char]:
+#                     matches += 1
+
+#             for r in range(len(t) + l, len(s) + 1):
+#                 substr = s[l : r]
+#                 print(substr, matches)
+
+#                 if r != len(t) + l:
+#                     smap[substr[-1]] = smap[substr[-1]] + 1
+#                     if smap[substr[-1]] == tmap[substr[-1]]: 
+#                         matches += 1
+                
+#                 if matches == 52 and len(substr) <= shortest:
+#                     shortest = len(substr)
+#                     shortestStr = substr
+#                     print("shortestStr:", shortestStr, "shortest:", shortest)
+#                     if shortest == len(t):
+#                         return shortestStr
+
+#         return shortestStr
