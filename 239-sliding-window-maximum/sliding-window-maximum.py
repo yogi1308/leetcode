a367a09aa1e320c67c105790701249fb6428d411
yogi1308 <shreetej.yogi@gmail.com>
2026-01-06 11:12:03 -0700

    Time: 261 ms (19.03%) | Memory: 34.6 MB (14.62%) - LeetSync

--- 239-sliding-window-maximum/sliding-window-maximum.py
+++ 239-sliding-window-maximum/sliding-window-maximum.py
@@ -1,11 +1,26 @@
 class Solution:
     def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
-        heap = []
-        output = []
-        for i in range(len(nums)):
-            heapq.heappush(heap, (-nums[i], i))
-            if i >= k - 1:
-                while heap[0][1] <= i - k:
-                    heapq.heappop(heap)
-                output.append(-heap[0][0])
+        n = len(nums)
+        leftMax = [0] * n
+        rightMax = [0] * n
+
+        leftMax[0] = nums[0]
+        rightMax[n - 1] = nums[n - 1]
+
+        for i in range(1, n):
+            if i % k == 0:
+                leftMax[i] = nums[i]
+            else:
+                leftMax[i] = max(leftMax[i - 1], nums[i])
+
+            if (n - 1 - i) % k == 0:
+                rightMax[n - 1 - i] = nums[n - 1 - i]
+            else:
+                rightMax[n - 1 - i] = max(rightMax[n - i], nums[n - 1 - i])
+
+        output = [0] * (n - k + 1)
+
+        for i in range(n - k + 1):
+            output[i] = max(leftMax[i + k - 1], rightMax[i])
+
         return output
