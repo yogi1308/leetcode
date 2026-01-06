b62ea8f341d8f0398548e7849f58334da93af36b
yogi1308 <shreetej.yogi@gmail.com>
2026-01-06 11:14:07 -0700

    Time: 155 ms (79.98%) | Memory: 36.5 MB (11.50%) - LeetSync

--- 239-sliding-window-maximum/sliding-window-maximum.py
+++ 239-sliding-window-maximum/sliding-window-maximum.py
@@ -1,17 +1,26 @@
+from collections import deque
 class Solution:
     def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
-        if len(nums) < k:
-            return []
-        if k == 1:
-            return nums
-        dp = collections.deque()
-        result = []
-        for i in range(len(nums)):
-            while dp and dp[0] < i-k+1:
-                dp.popleft()
-            while dp and nums[dp[-1]] < nums[i]:
-                dp.pop()
-            dp.append(i)
-            if i >= k-1:
-                result.append(nums[dp[0]])
-        return result
+        # we need to keep track of the max number in window of k
+        # sort of like a CNN kernel
+        # this is a monotonic queue
+        q = deque()
+        # when we add a new element, we pop right until we see a >= elem
+        # then, when we move the window, we pop left one if the elems are equal.
+        # the biggest element will be on the left. 
+        for num in nums[:k]:
+            while q and q[-1] < num:
+                q.pop()
+            q.append(num)
+
+        out = [q[0]]
+
+        for i, num in enumerate(nums[k:]):
+            if q[0] == nums[i]:
+                q.popleft()
+            while q and q[-1] < num:
+                q.pop()
+            q.append(num)
+            out.append(q[0])
+        
+        return out
