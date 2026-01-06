d0cc9e98664d4f9b2e4967b915a280090f1edc39
yogi1308 <shreetej.yogi@gmail.com>
2026-01-06 11:12:31 -0700

    Time: 195 ms (29.87%) | Memory: 34.8 MB (14.23%) - LeetSync

--- 239-sliding-window-maximum/sliding-window-maximum.py
+++ 239-sliding-window-maximum/sliding-window-maximum.py
@@ -1,26 +1,18 @@
 class Solution:
     def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
-        n = len(nums)
-        leftMax = [0] * n
-        rightMax = [0] * n
+        output_array = []
+        q = collections.deque()
+        l = r = 0
+        while r < len(nums):
+            while q and nums[q[-1]] < nums[r]:
+                q.pop()
+            q.append(r)
 
-        leftMax[0] = nums[0]
-        rightMax[n - 1] = nums[n - 1]
+            if l > q[0]:
+                q.popleft()
 
-        for i in range(1, n):
-            if i % k == 0:
-                leftMax[i] = nums[i]
-            else:
-                leftMax[i] = max(leftMax[i - 1], nums[i])
-
-            if (n - 1 - i) % k == 0:
-                rightMax[n - 1 - i] = nums[n - 1 - i]
-            else:
-                rightMax[n - 1 - i] = max(rightMax[n - i], nums[n - 1 - i])
-
-        output = [0] * (n - k + 1)
-
-        for i in range(n - k + 1):
-            output[i] = max(leftMax[i + k - 1], rightMax[i])
-
-        return output
+            if (r + 1) >= k:
+                output_array.append(nums[q[0]])
+                l += 1
+            r += 1
+        return output_array
