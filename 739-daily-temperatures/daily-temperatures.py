bbca8a3eec33859bb587e54280f51ac8ad9606df
yogi1308 <shreetej.yogi@gmail.com>
2026-01-07 11:24:43 -0700

    Time: 93 ms (63.83%) | Memory: 28.7 MB (34.50%) - LeetSync

--- 739-daily-temperatures/daily-temperatures.py
+++ 739-daily-temperatures/daily-temperatures.py
@@ -3,16 +3,13 @@ class Solution:
         result = [0] * len(temperatures)
         stack = []
         for i in range(len(temperatures)):
-            if not stack or temperatures[i] <= temperatures[stack[-1]]:
-                stack.append(i)
-                continue
-
             while stack and temperatures[i] > temperatures[stack[-1]]:
                 popped_index = stack.pop()
                 result[popped_index] = i - popped_index
             stack.append(i)
 
         return result
+
 
 # original brute force solution
 # class Solution:
