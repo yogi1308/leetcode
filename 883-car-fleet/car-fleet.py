4f07fe30afa16a6a558ef18859b111d44f6b0eec
yogi1308 <shreetej.yogi@gmail.com>
2026-01-08 10:56:46 -0700

    Time: 259 ms (22.14%) | Memory: 40.8 MB (25.69%) - LeetSync

--- 883-car-fleet/car-fleet.py
+++ 883-car-fleet/car-fleet.py
@@ -2,9 +2,11 @@ class Solution:
     def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
         pos_speed = [[pos, sp] for pos, sp in zip(position, speed)]
         pos_speed.sort(reverse=True)
-        stack = []
+        fleet = 0
+        prev_time = 0
         for el in pos_speed:
             time = (target - el[0])/el[1]
-            if not stack or time > stack[-1]:
-                stack.append((target - el[0])/el[1])
-        return len(stack)
+            if prev_time == 0 or time > prev_time:
+                fleet += 1
+                prev_time = (target - el[0])/el[1]
+        return fleet
