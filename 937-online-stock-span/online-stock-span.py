0765d17120061d8af82d5811a9bf85fc742477aa
yogi1308 <shreetej.yogi@gmail.com>
2026-01-12 13:17:56 -0700

    Time: 74 ms (31.75%) | Memory: 25.5 MB (7.26%) - LeetSync

--- 937-online-stock-span/online-stock-span.py
+++ 937-online-stock-span/online-stock-span.py
@@ -6,13 +6,11 @@ class StockSpanner:
 
     def next(self, price: int) -> int:
         curr = -1
-        while True:
-            if not self.stack or abs(curr) > len(self.stack) or price < self.stack[curr][0]:
-                self.stack.append((price, abs(curr)))
-                return abs(curr)
-            else:
-                curr -= self.stack[curr][1]
+        while self.stack and abs(curr) <= len(self.stack) and price >= self.stack[curr][0]:
+            curr -= self.stack[curr][1]
             
+        self.stack.append((price, abs(curr)))
+        return abs(curr)
 
         
 
