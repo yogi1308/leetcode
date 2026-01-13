c7535ce7cdc7f9437277e8c244f480c8ffe99488
yogi1308 <shreetej.yogi@gmail.com>
2026-01-13 11:09:50 -0700

    Time: 0 ms (100.00%) | Memory: 19.5 MB (5.83%) - LeetSync

--- 71-simplify-path/simplify-path.py
+++ 71-simplify-path/simplify-path.py
@@ -4,6 +4,6 @@ class Solution:
         stack = []
         for el in path:
             if not el or el == '.' or not stack and el == '..': pass
-            elif stack and el == '..': stack.pop()
+            elif el == '..': stack.pop()
             else: stack.append(el)
         return '/' + '/'.join(stack)
