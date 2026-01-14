8c098ba2153c9cf32d3eb7ab8a358c83c9ba6049
yogi1308 <shreetej.yogi@gmail.com>
2026-01-14 12:31:28 -0700

    Time: 63 ms (87.14%) | Memory: 27.9 MB (9.74%) - LeetSync

--- 931-maximum-frequency-stack/maximum-frequency-stack.py
+++ 931-maximum-frequency-stack/maximum-frequency-stack.py
@@ -1,36 +1,19 @@
 class FreqStack:
 
     def __init__(self):
-        self.vals = []
-        self.count_group = {}
-        max_count = 0
-        
+        self.cnt = {}
+        self.stacks = [[]]
 
     def push(self, val: int) -> None:
-        self.vals.append(val)
-        val_count = self.vals.count(val)
-        if val_count in self.count_group:
-            self.count_group[val_count].append(val)
-        else:
-            self.count_group[val_count] = [val]
-            self.max_count = val_count
+        valCnt = 1 + self.cnt.get(val, 0)
+        self.cnt[val] = valCnt
+        if valCnt == len(self.stacks):
+            self.stacks.append([])
+        self.stacks[valCnt].append(val)
 
-
-        
     def pop(self) -> int:
-        popped_value = self.count_group[self.max_count].pop()
-        if len(self.count_group[self.max_count]) == 0: 
-            self.count_group.pop(self.max_count)
-            self.max_count -= 1
-        for i in range(len(self.vals) - 1, -1, -1):
-            if self.vals[i] == popped_value:
-                self.vals.pop(i)
-                break
-        return popped_value
-        
-
-
-# Your FreqStack object will be instantiated and called as such:
-# obj = FreqStack()
-# obj.push(val)
-# param_2 = obj.pop()
+        res = self.stacks[-1].pop()
+        self.cnt[res] -= 1
+        if not self.stacks[-1]:
+            self.stacks.pop()
+        return res
