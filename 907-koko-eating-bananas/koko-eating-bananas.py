3e7f360efb0c36ecbcbe517c741ba9250f0b8250
Shreetej Hadge <shreetej.yogi@gmail.com>
2026-01-19 12:45:11 -0700

    Time: 189 ms (21.34%) | Memory: 20.8 MB (12.11%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.py
+++ 907-koko-eating-bananas/koko-eating-bananas.py
@@ -6,6 +6,7 @@ class Solution:
             sum_val = 0
             for bananas in piles:
                 sum_val += math.ceil(bananas/test)
+                if sum_val > h: break
             if sum_val <= h:
                 high = test - 1
             else:
