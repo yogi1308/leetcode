9d002023560f23347f723bca7c358a958d5b959c
Shreetej Hadge <shreetej.yogi@gmail.com>
2026-01-19 11:01:34 -0700

    Time: 0 ms (100.00%) | Memory: 19.8 MB (9.03%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.py
+++ 74-search-a-2d-matrix/search-a-2d-matrix.py
@@ -1,6 +1,5 @@
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
-
         while matrix:
             mid = matrix[round(len(matrix) / 2)]
             if target < mid[0]:
@@ -9,13 +8,11 @@ class Solution:
                 matrix = matrix[round(len(matrix) / 2) + 1 :]
             else:
                 matrix = mid
-                break
-
-        while matrix:
-            if target < matrix[round(len(matrix) / 2)]:
-                matrix = matrix[:round(len(matrix) / 2)]
-            elif target > matrix[round(len(matrix) / 2)]:
-                matrix = matrix[(round(len(matrix) / 2)) + 1:]
-            elif target == matrix[round(len(matrix) /2)]:
-                return True
+                while matrix:
+                    if target < matrix[round(len(matrix) / 2)]:
+                        matrix = matrix[:round(len(matrix) / 2)]
+                    elif target > matrix[round(len(matrix) / 2)]:
+                        matrix = matrix[(round(len(matrix) / 2)) + 1:]
+                    elif target == matrix[round(len(matrix) /2)]:
+                        return True
         return False
