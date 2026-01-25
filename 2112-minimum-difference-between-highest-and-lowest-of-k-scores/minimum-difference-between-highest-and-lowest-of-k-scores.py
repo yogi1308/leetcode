534d1f191f3d0a41f46286fc8ce2e7feb47a2737
Shreetej Hadge <shreetej.yogi@gmail.com>
2026-01-24 21:32:08 -0700

    Time: 101 ms (5.18%) | Memory: 19.5 MB (15.85%) - LeetSync

--- 2112-minimum-difference-between-highest-and-lowest-of-k-scores/minimum-difference-between-highest-and-lowest-of-k-scores.py
+++ 2112-minimum-difference-between-highest-and-lowest-of-k-scores/minimum-difference-between-highest-and-lowest-of-k-scores.py
@@ -1,10 +1,11 @@
 class Solution:
     def minimumDifference(self, nums: List[int], k: int) -> int:
-        nums.sort()
-        l, r = 0, k - 1
-        res = float("inf")
-        while r < len(nums):
-            res = min(res, nums[r] - nums[l])
-            l += 1
-            r += 1
-        return res
+        l, r = 0, k
+        nums = sorted(nums)
+        min_diff = max(nums)
+        while r < len(nums)  + 1:
+            arr = nums[l : r]
+            min_diff = min(min_diff, max(arr) - min(arr))
+            l, r = l + 1, r + 1
+        return min_diff
+                
