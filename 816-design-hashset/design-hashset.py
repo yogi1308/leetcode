07833994e73dedf490a61f06e81cb8b71bd89ab8
Shreetej Hadge <shreetej.yogi@gmail.com>
2026-02-01 12:55:52 -0700

    Time: 61 ms (43.37%) | Memory: 25.6 MB (30.05%) - LeetSync

--- 816-design-hashset/design-hashset.py
+++ 816-design-hashset/design-hashset.py
@@ -1,20 +1,42 @@
+class ListNode:
+    def __init__(self, val, next = None) -> None:
+        self.val = val
+        self.next = next
+
 class MyHashSet:
 
-    def __init__(self):
-        self.vals = []
+    def __init__(self, length = 10000):
+        self.set = [ListNode(0) for i in range(length)]
+        self.length = length
         
 
     def add(self, key: int) -> None:
-        if key not in self.vals: self.vals.append(key)
+        idx = key % self.length
+        curr = self.set[idx]
+        while curr.next:
+            if curr.next.val == key: return 
+            curr = curr.next
+        curr.next = ListNode(key)
         
 
     def remove(self, key: int) -> None:
-        if self.contains(key):
-            self.vals.remove(key)
+        idx = key % self.length
+        curr = self.set[idx]
+        while curr.next:
+            if curr.next.val == key: 
+                curr.next = curr.next.next
+                return
+            curr = curr.next
+        
 
     def contains(self, key: int) -> bool:
-        return True if key in self.vals else False
-
+        idx = key % self.length
+        curr = self.set[idx]
+        while curr.next:
+            if curr.next.val == key: 
+                return True
+            curr = curr.next
+        return False
         
 
 
