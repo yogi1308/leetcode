3c7f1384791223914511fad62f84e5f51d7c9d11
Shreetej Hadge <shreetej.yogi@gmail.com>
2026-02-01 12:21:21 -0700

    Time: 3 ms (85.08%) | Memory: 21.5 MB (10.37%) - LeetSync

--- 169-majority-element/majority-element.py
+++ 169-majority-element/majority-element.py
@@ -1,11 +1,4 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        if len(nums) == 1: return nums[0]
-        dictionary  = {}
-        mark = len(nums) / 2
-        for num in nums:
-            if num not in dictionary:
-                dictionary[num] = 1
-            else:
-                dictionary[num] += 1
-                if dictionary[num] > mark: return num
+        nums.sort()
+        return nums[len(nums) // 2]
