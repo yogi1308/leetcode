9c1de4124430dc80b87fccd5e459e1603146c863
Shreetej Hadge <shreetej.yogi@gmail.com>
2026-02-21 13:00:34 -0700

    Time: 15 ms (12.11%) | Memory: 22.3 MB (48.13%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.py
+++ 229-majority-element-ii/majority-element-ii.py
@@ -1,13 +1,22 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> List[int]:
-        map = {}
-        res = []
-        for i in range(len(nums)):
-            if nums[i] not in map:
-                map[nums[i]] = 0
-            map[nums[i]] = map[nums[i]] + 1
-        for num in map:
-            if map[num] > math.floor(len(nums)/3):
-                res.append(num)
-        return res
+        count = defaultdict(int)
 
+        for num in nums:
+            count[num] += 1
+
+            if len(count) <= 2:
+                continue
+
+            new_count = defaultdict(int)
+            for num, c in count.items():
+                if c > 1:
+                    new_count[num] = c - 1
+            count = new_count
+
+        res = []
+        for num in count:
+            if nums.count(num) > len(nums) // 3:
+                res.append(num)
+
+        return res
