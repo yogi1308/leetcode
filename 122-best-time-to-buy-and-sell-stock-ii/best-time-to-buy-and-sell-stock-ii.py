084c3c9721422aceb343f8065bc2833f8c68872d
Shreetej Hadge <shreetej.yogi@gmail.com>
2026-02-21 12:28:25 -0700

    Time: 5 ms (13.97%) | Memory: 20.5 MB (34.44%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.py
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.py
@@ -1,21 +1,18 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         profit = 0
-        bought = float('inf')
+        bought = 0
         hold = False
         for i in range(len(prices)):
             if i != len(prices) - 1:
                 if prices[i] < prices[i + 1] and not hold:
                     bought = prices[i]
                     hold = True
-                    print("bought: ", bought)
-                elif bought < prices[i] and prices[i] > prices[i + 1]:
+                elif bought < prices[i] and prices[i] > prices[i + 1] and hold:
                     profit += prices[i] - bought
-                    bought = float('inf')
                     hold = False
-                    print("sold: ", prices[i] - bought, i, prices[i])
             else: 
-                if prices[i] > bought:
+                if prices[i] > bought and hold:
                     profit += prices[i] - bought
         return profit
 
