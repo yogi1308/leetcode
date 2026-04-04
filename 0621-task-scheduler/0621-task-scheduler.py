2ac2aec900db8efdbdd275242c35d824f5b87f7e
Shreetej Hadge <shreetej.yogi@gmail.com>
2026-04-03 22:15:09 -0700

    0621-TASK-SCHEDULER Stats: Time: 110 ms (55.57%), Space: 20.7 MB (91.15%) - LeetHub

--- 0621-task-scheduler/0621-task-scheduler.py
+++ 0621-task-scheduler/0621-task-scheduler.py
@@ -1,11 +1,7 @@
 class Solution:
     def leastInterval(self, tasks: List[str], n: int) -> int:
-        hmap = {}
-        for task in tasks:
-            hmap[task] = hmap.get(task, 0) + 1
-        heap = []
-        for task in hmap:
-            heap.append([hmap[task] * -1, task])
+        count = Counter(tasks)
+        heap = [-cnt for cnt in count.values()]
         heapq.heapify(heap)
 
         cycles = 0
@@ -19,9 +15,8 @@ class Solution:
                 gap -= 1
             
             for task in taskcompleted:
-                newtask = [(task[0] + 1), task[1]]
-                if newtask[0] != 0:
-                    heapq.heappush(heap, newtask)
+                if task != -1:
+                    heapq.heappush(heap, task + 1)
 
             while gap >= 0 and heap:
                 cycles += 1
