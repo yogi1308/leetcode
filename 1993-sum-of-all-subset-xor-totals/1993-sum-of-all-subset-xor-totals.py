bdfe4aeaa6d37b253650f290f155f073ce963b22
Shreetej Hadge <shreetej.yogi@gmail.com>
2026-05-11 14:15:19 -0700

    1993-SUM-OF-ALL-SUBSET-XOR-TOTALS Stats: Time: 8 ms (72.32%), Space: 19.2 MB (65.41%) - LeetHub

--- 1993-sum-of-all-subset-xor-totals/1993-sum-of-all-subset-xor-totals.py
+++ 1993-sum-of-all-subset-xor-totals/1993-sum-of-all-subset-xor-totals.py
@@ -1,38 +1,7 @@
 class Solution:
     def subsetXORSum(self, nums: List[int]) -> int:
-        # global parameters
-        subset = []
-        idx = 0
-
-        solution = []
-
-        def visitAll():
-            # without `nonlocal` we'd just be setting local variables
-            nonlocal idx, subset, solution
-
-            # base case
-            if idx == len(nums):
-                # `subset` is global and will change if we don't copy it
-                solution.append(subset.copy())
-                return
-
-            # recursion
-            
-            idx += 1 # add 1
-            visitAll()
-            idx -= 1 # undo
-
-            subset.append(nums[idx]) # add nums[idx]
-            idx += 1 # add 1
-            visitAll()
-            idx -= 1 # undo 
-            subset.pop() # undo
-
-        visitAll()
-        total = 0
-        for subset in solution:
-            val = 0
-            for num in subset:
-                val ^= num
-            total += val
-        return total
+        def dfs(i, total):
+            if i == len(nums):
+                return total
+            return dfs(i + 1, total ^ nums[i]) + dfs(i + 1, total)
+        return dfs(0, 0)
