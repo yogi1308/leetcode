184ac03ac854734560ecd0748ae63d8d96b34035
Shreetej Hadge <shreetej.yogi@gmail.com>
2026-05-18 15:37:39 -0700

    0698-PARTITION-TO-K-EQUAL-SUM-SUBSETS Stats: Time: 8214 ms (5.04%), Space: 19.5 MB (53.21%) - LeetHub

--- 0698-partition-to-k-equal-sum-subsets/0698-partition-to-k-equal-sum-subsets.py
+++ 0698-partition-to-k-equal-sum-subsets/0698-partition-to-k-equal-sum-subsets.py
@@ -1,22 +1,25 @@
 class Solution:
     def canPartitionKSubsets(self, nums: List[int], k: int) -> bool:
-        total = sum(nums)
-        subsetTotal = sum(nums) // k
-        subset = [0] * k
-        if subsetTotal != total / k:
+        if sum(nums) % k != 0:
             return False
+
         nums.sort(reverse=True)
-        def backtrack(i):
-            if i == len(nums):
+        target = sum(nums) // k
+        used = [False] * len(nums)
+
+        if nums[0] > target: return False
+        def backtrack(i, k, subsetSum):
+            if k == 0:
                 return True
-            
-            seen = set()
-            for j in range(k):
-                if subset[j] not in seen and subset[j] + nums[i] <= subsetTotal:
-                    seen.add(subset[j])
-                    subset[j] += nums[i]
-                    if backtrack(i + 1):
-                        return True
-                    subset[j] -= nums[i]
+            if subsetSum == target:
+                return backtrack(0, k - 1, 0)
+            for j in range(i, len(nums)):
+                if used[j] or subsetSum + nums[j] > target:
+                    continue
+                used[j] = True
+                if backtrack(j + 1, k, subsetSum + nums[j]):
+                    return True
+                used[j] = False
             return False
-        return backtrack(0)
+
+        return backtrack(0, k, 0)
