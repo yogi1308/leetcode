fe3b8f67cd13c6edf8501600a0e52290774103b1
Shreetej Hadge <shreetej.yogi@gmail.com>
2026-05-22 16:29:26 -0700

    0212-WORD-SEARCH-II Stats: Time: 4170 ms (64.05%), Space: 21.7 MB (22.57%) - LeetHub

--- 0212-word-search-ii/0212-word-search-ii.py
+++ 0212-word-search-ii/0212-word-search-ii.py
@@ -2,6 +2,7 @@ class TrieNode:
     def __init__(self) -> None:
         self.children = {}
         self.end = False
+        self.word = ""
 
 class Solution:
     def findWords(self, board: List[List[str]], words: List[str]) -> List[str]:
@@ -15,30 +16,29 @@ class Solution:
                     curr.children[c] = TrieNode()
                 curr = curr.children[c]
             curr.end = True
+            curr.word = word
 
         for word in words:
             addWord(word)
 
-        def dfs(x, y, occupied, curr, word):
+        def dfs(x, y, occupied, curr):
             if (x, y) in occupied: return
-            
-            word += board[y][x]
             if curr.end: 
-                res.add(word[:])
+                res.add(curr.word)
                 
             occupied.add((x, y))
             if x - 1 >= 0:
                 if board[y][x-1] in curr.children: 
-                    dfs(x-1, y, occupied, curr.children[board[y][x-1]], word)
+                    dfs(x-1, y, occupied, curr.children[board[y][x-1]])
             if x + 1 < len(board[0]):
                 if board[y][x + 1] in curr.children: 
-                    dfs(x + 1, y, occupied, curr.children[board[y][x + 1]], word)
+                    dfs(x + 1, y, occupied, curr.children[board[y][x + 1]])
             if y - 1 >= 0:
                 if board[y-1][x] in curr.children: 
-                    dfs(x, y-1, occupied, curr.children[board[y-1][x]], word)
+                    dfs(x, y-1, occupied, curr.children[board[y-1][x]])
             if y + 1 < len(board):
                 if board[y+1][x] in curr.children: 
-                    dfs(x, y+1, occupied, curr.children[board[y+1][x]], word)
+                    dfs(x, y+1, occupied, curr.children[board[y+1][x]])
             
             occupied.remove((x,y))
             return
@@ -46,7 +46,7 @@ class Solution:
         for y in range(len(board)):
             for x in range(len(board[y])):
                 if board[y][x] in root.children:
-                    dfs(x, y, set(), root.children[board[y][x]], "")
+                    dfs(x, y, set(), root.children[board[y][x]])
 
         return list(res)
 
