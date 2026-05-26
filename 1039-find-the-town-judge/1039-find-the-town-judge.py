8fc66b633c03aafc56fca1515b1ffa97a6ba14da
Shreetej Hadge <shreetej.yogi@gmail.com>
2026-05-26 13:00:47 -0700

    1039-FIND-THE-TOWN-JUDGE Stats: Time: 31 ms (7.85%), Space: 22.5 MB (82.87%) - LeetHub

--- 1039-find-the-town-judge/1039-find-the-town-judge.py
+++ 1039-find-the-town-judge/1039-find-the-town-judge.py
@@ -1,15 +1,13 @@
 class Solution:
     def findJudge(self, n: int, trust: List[List[int]]) -> int:
-        table = {i: set() for i in range(1, n + 1)}
-        for i in trust: 
-            table[i[0]].add(i[1])
-        candidate = -1
-        for i in table:
-            if table[i] == set(): 
-                candidate = i
-                break
-        for i in table:
-            if i == candidate: continue
-            if candidate not in table[i]:
-                return -1
-        return candidate
+        delta = defaultdict(int)
+
+        for src, dst in trust:
+            delta[src] -= 1
+            delta[dst] += 1
+
+        for i in range(1, n + 1):
+            if delta[i] == n - 1:
+                return i
+
+        return -1
