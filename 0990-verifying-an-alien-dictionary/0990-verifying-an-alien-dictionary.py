678b547c4e47f6eacee5d078caf54ed4f23f64a7
Shreetej Hadge <shreetej.yogi@gmail.com>
2026-05-26 11:24:03 -0700

    0990-VERIFYING-AN-ALIEN-DICTIONARY Stats: Time: 0 ms (100%), Space: 19.3 MB (74.36%) - LeetHub

--- 0990-verifying-an-alien-dictionary/0990-verifying-an-alien-dictionary.py
+++ 0990-verifying-an-alien-dictionary/0990-verifying-an-alien-dictionary.py
@@ -1,10 +1,6 @@
 class Solution:
     def isAlienSorted(self, words: List[str], order: str) -> bool:
         orderTable = {order[c]: c for c in range(len(order))}
-        def appearsBefore(c1, c2):
-            if orderTable[c1] <= orderTable[c2]: return True
-            return False
-
         for i in range(len(words) - 1):
             word1 = words[i]
             word2 = words[i + 1]
@@ -14,7 +10,7 @@ class Solution:
                 c2 = word2[j]
                 if c1 == c2: continue
                 same = False
-                if appearsBefore(c1, c2): break
+                if orderTable[c1] <= orderTable[c2]: break
                 else: 
                     return False
             if same:
