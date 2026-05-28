b52bf65e9ac1726cef00fb4335d79302277e67aa
Shreetej Hadge <shreetej.yogi@gmail.com>
2026-05-28 09:57:04 -0700

    0200-NUMBER-OF-ISLANDS Stats: Time: 213 ms (99.8%), Space: 21.5 MB (73.11%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,23 +1,22 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
         res = 0
-        addedLand = set()
 
         def dfs(x, y):
-            addedLand.add((x, y))
-            if x-1 >= 0 and grid[y][x-1] == "1" and (x-1, y) not in addedLand:
+            grid[y][x] = "0"
+            if x-1 >= 0 and grid[y][x-1] == "1":
                 dfs(x-1, y)
-            if y-1 >= 0 and grid[y-1][x] == "1" and (x, y-1) not in addedLand:
+            if y-1 >= 0 and grid[y-1][x] == "1":
                 dfs(x, y-1)
-            if x+1 < len(grid[y]) and grid[y][x+1] == "1" and (x+1, y) not in addedLand:
+            if x+1 < len(grid[y]) and grid[y][x+1] == "1":
                 dfs(x+1, y)
-            if y+1 < len(grid) and grid[y+1][x] == "1" and (x, y+1) not in addedLand:
+            if y+1 < len(grid) and grid[y+1][x] == "1":
                 dfs(x, y+1)
             return
 
         for y in range(len(grid)):
             for x in range(len(grid[y])):
-                if grid[y][x] == "1" and (x, y) not in addedLand:
+                if grid[y][x] == "1" :
                     dfs(x, y)
                     res += 1
         
