06681190f5578f7b725108ab0feb22fef35dcf1a
Shreetej Hadge <shreetej.yogi@gmail.com>
2026-06-05 10:54:32 -0700

    0207-COURSE-SCHEDULE Stats: Time: 4 ms (64.67%), Space: 21.4 MB (15.06%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.py
+++ 0207-course-schedule/0207-course-schedule.py
@@ -2,26 +2,25 @@ class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
         hmap = {}
         dfsDone = set()
-        found = False
         def dfs(prereqs, visited):
-            nonlocal found
-            if found: return 
             for course in prereqs:
                 if course in visited:
-                    found = True
+                    return False
                 elif course in dfsDone: continue
                 else: 
                     visited.add(course)
-                    if course in hmap: dfs(hmap[course], visited)
+                    found = False
+                    if course in hmap:
+                        if not dfs(hmap[course], visited): return False
                     dfsDone.add(course)
                     visited.remove(course)
+            return True
 
         for course, prereq in prerequisites:
             hmap.setdefault(course, []).append(prereq)
 
         for prereq in hmap:
             if prereq not in dfsDone:
-                dfs(hmap[prereq], {prereq})
-                if found: return False
+                if not dfs(hmap[prereq], {prereq}): return False
 
         return True
